3541a5c61229d385efcd8bc99a6d7dff8087e790
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-12 18:55:09 +0530

    add basic layout with header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>Calculator View</>} />
-        <Route path="/edit" element={<>Calculator Editor</>} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<>Calculator View</>} />
+          <Route path="/edit" element={<>Calculator Editor</>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
