55b85042580307a7eea77fd34f74bf3dfb47dd4b
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-12 20:32:48 +0530

    update component ids

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -2,33 +2,33 @@ import { Divide, Hash, Minus, Plus, XIcon } from "lucide-react";
 import type { Component } from "../types";
 
 export const ALL_COMPONENTS: Component[] = [
-  { id: crypto.randomUUID(), type: "display", value: "Result Display" },
+  { id: "display", type: "display", value: "Result Display" },
   {
-    id: crypto.randomUUID(),
+    id: "add",
     type: "operator",
     value: "Add",
     icon: <Plus className="w-4 h-4 text-gray-400" />,
   },
   {
-    id: crypto.randomUUID(),
+    id: "subtract",
     type: "operator",
     value: "Subtract",
     icon: <Minus className="w-4 h-4 text-gray-400" />,
   },
   {
-    id: crypto.randomUUID(),
+    id: "multiply",
     type: "operator",
     value: "Multiply",
     icon: <XIcon className="w-4 h-4 text-gray-400" />,
   },
   {
-    id: crypto.randomUUID(),
+    id: "divide",
     type: "operator",
     value: "Divide",
     icon: <Divide className="w-4 h-4 text-gray-400" />,
   },
   ...Array.from({ length: 10 }).map((_, i) => ({
-    id: crypto.randomUUID(),
+    id: i.toString(),
     type: "number",
     value: i.toString(),
     icon: <Hash className="w-3 h-3 text-gray-400" />,
