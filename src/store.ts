3b948902067813be9ea94c90af9d1aecb6921646
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-13 15:19:38 +0530

    add localstorage persistence

--- src/store.ts
+++ src/store.ts
@@ -1,36 +1,46 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import { combine, persist } from "zustand/middleware";
 import type { Component } from "./types";
 import { ALL_COMPONENTS } from "./lib/data";
 
 export const useEditorStore = create(
-  combine({ editorComponents: <Component[]>[] }, (set) => ({
-    setEditorComponents: (components: Component[]) =>
-      set(() => ({ editorComponents: components })),
-    addEditorComponent: (newComponent: Component) =>
-      set((state) => ({
-        editorComponents: [...state.editorComponents, newComponent],
-      })),
-    removeEditorComponent: (id: string) =>
-      set((state) => ({
-        editorComponents: state.editorComponents.filter(
-          (item) => item.id !== id
-        ),
-      })),
-  }))
+  persist(
+    combine({ editorComponents: <Component[]>[] }, (set) => ({
+      setEditorComponents: (components: Component[]) =>
+        set(() => ({ editorComponents: components })),
+      addEditorComponent: (newComponent: Component) =>
+        set((state) => ({
+          editorComponents: [...state.editorComponents, newComponent],
+        })),
+      removeEditorComponent: (id: string) =>
+        set((state) => ({
+          editorComponents: state.editorComponents.filter(
+            (item) => item.id !== id
+          ),
+        })),
+    })),
+    {
+      name: "editor-storage",
+    }
+  )
 );
 
 export const useSidebarStore = create(
-  combine({ sidebarComponents: [...ALL_COMPONENTS] }, (set) => ({
-    addSidebarComponent: (newComponent: Component) =>
-      set((state) => ({
-        sidebarComponents: [...state.sidebarComponents, newComponent],
-      })),
-    removeSidebarComponent: (id: string) =>
-      set((state) => ({
-        sidebarComponents: state.sidebarComponents.filter(
-          (item) => item.id !== id
-        ),
-      })),
-  }))
+  persist(
+    combine({ sidebarComponents: [...ALL_COMPONENTS] }, (set) => ({
+      addSidebarComponent: (newComponent: Component) =>
+        set((state) => ({
+          sidebarComponents: [...state.sidebarComponents, newComponent],
+        })),
+      removeSidebarComponent: (id: string) =>
+        set((state) => ({
+          sidebarComponents: state.sidebarComponents.filter(
+            (item) => item.id !== id
+          ),
+        })),
+    })),
+    {
+      name: "sidebar-storage",
+    }
+  )
 );
