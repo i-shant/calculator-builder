7f7b34232a907791a4de3149da0c85c4b8a3647b
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-13 11:05:53 +0530

    logic to add components from sidebar to editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
 import Sidebar from "./Sidebar";
+import { Component } from "../types";
 
 export default function Editor() {
+  const [components, setComponents] = useState<Component[]>([]);
+
+  function addComponent(newComponent: Component) {
+    setComponents([...components, newComponent]);
+  }
+
+  function renderComponent(item: Component) {
+    switch (item.type) {
+      case "display":
+        return (
+          <div className="col-span-full bg-orange-50 p-4 rounded text-2xl text-end shadow-sm font-semibold">
+            0
+          </div>
+        );
+      case "operator":
+        return (
+          <button className="flex items-center justify-center bg-white/50 rounded shadow p-4 cursor-pointer hover:bg-white">
+            {item.icon}
+          </button>
+        );
+      case "number":
+        return (
+          <button className="flex items-center justify-center bg-cyan-50 rounded shadow-sm p-3 text-lg text-gray-600 font-semibold cursor-pointer hover:bg-white">
+            {item.value}
+          </button>
+        );
+      case "default":
+        return null;
+    }
+  }
+
   return (
     <div className="flex flex-col sm:flex-row flex-1 gap-2">
-      <Sidebar />
-      <main>main</main>
+      <Sidebar handleClick={addComponent} />
+
+      <main className="p-4 text-gray-800 flex-1">
+        <h2 className="text font-bold">Editor</h2>
+        <div className="w-full flex items-center justify-center">
+          <div className="p-4 m-4 w-96 border-2 border-dashed border-gray-400 rounded">
+            <div className="grid grid-cols-4 gap-2">
+              {components.map((item) => renderComponent(item))}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,11 @@
 import { ALL_COMPONENTS } from "../lib/data";
+import { Component } from "../types";
 
-export default function Sidebar() {
+type Props = {
+  handleClick: (newComponent: Component) => void;
+};
+
+export default function Sidebar({ handleClick }: Props) {
   return (
     <aside className="p-4 border-b sm:border-r sm:border-b-0 border-gray-300 text-gray-800">
       <h2 className="text font-bold">Components</h2>
@@ -10,6 +15,7 @@ export default function Sidebar() {
           return (
             <button
               key={item.id}
+              onClick={() => handleClick(item)}
               className={`${
                 item.type === "display" ? "col-span-3 sm:col-span-2" : ""
               } flex items-center justify-center gap-1 border border-gray-300 bg-white/50 px-3 py-2 rounded shadow cursor-pointer transition-shadow hover:bg-white hover:shadow-none focus:bg-white focus:shadow-none`}
