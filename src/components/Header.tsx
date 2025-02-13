af625621686d1fa4e6272cc179472cbee842cb31
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-13 17:05:58 +0530

    update header to support dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,13 +6,16 @@ export default function Header() {
   const { theme, toggleTheme } = useThemeStore();
 
   return (
-    <header className="bg-white shadow">
+    <header className="bg-white shadow dark:bg-gray-400">
       <div className="container mx-auto p-4">
         <div className="flex items-center justify-between">
-          <div className="text-xl font-semibold text-gray-800 sm:text-2xl">
+          <div className="text-xl font-semibold text-gray-800 sm:text-2xl dark:text-gray-100">
             <Link to="/">Calculator Builder</Link>
           </div>
-          <button onClick={toggleTheme} className="cursor-pointer">
+          <button
+            onClick={toggleTheme}
+            className="cursor-pointer text-gray-800 dark:text-gray-100"
+          >
             {theme === "dark" ? <Sun /> : <Moon />}
           </button>
         </div>
