39168026c4d06a7b4978307347e770ea5e634e69
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-13 16:57:01 +0530

    add dark mode toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router";
+import { useThemeStore } from "../store";
+import { Moon, Sun } from "lucide-react";
 
 export default function Header() {
+  const { theme, toggleTheme } = useThemeStore();
+
   return (
     <header className="bg-white shadow">
       <div className="container mx-auto p-4">
@@ -8,6 +12,9 @@ export default function Header() {
           <div className="text-xl font-semibold text-gray-800 sm:text-2xl">
             <Link to="/">Calculator Builder</Link>
           </div>
+          <button onClick={toggleTheme} className="cursor-pointer">
+            {theme === "dark" ? <Sun /> : <Moon />}
+          </button>
         </div>
       </div>
     </header>
