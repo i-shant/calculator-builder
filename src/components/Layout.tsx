5450c51270987bdbceb8b173375c8a287462b375
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-13 17:04:05 +0530

    update Layout to support dark mode

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
+    <div className="min-h-screen bg-gray-100 flex flex-col dark:bg-gray-800">
       <Header />
       <Outlet />
     </div>
