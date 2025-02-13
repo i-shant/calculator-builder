e7057f845b558bbf794b64f63a757de2d1518e51
Ishant Bhurani <188859462+i-shant@users.noreply.github.com>
2025-02-13 11:55:44 +0530

    simplify Editor component by creating a dedicated SortableArea component

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,56 +1,13 @@
 import { useState } from "react";
+import type { Component } from "../types";
 import Sidebar from "./Sidebar";
-import { Component } from "../types";
-import {
-  DndContext,
-  DragEndEvent,
-  DragOverlay,
-  DragStartEvent,
-  PointerSensor,
-  TouchSensor,
-  useSensor,
-  useSensors,
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  rectSortingStrategy,
-  SortableContext,
-} from "@dnd-kit/sortable";
-import SortableItem from "./SortableItem";
+import SortableArea from "./SortableArea";
 
 export default function Editor() {
   const [components, setComponents] = useState<Component[]>([]);
-  const [active, setActive] = useState<Component | null>(null);
-
-  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
 
   function addComponent(newComponent: Component) {
     setComponents([...components, newComponent]);
-  }
-
-  function handleDragStart(event: DragStartEvent) {
-    const { active } = event;
-
-    setActive(components.find((item) => item.id === active.id) ?? null);
-  }
-
-  function handleDragEnd(event: DragEndEvent) {
-    const { active, over } = event;
-
-    if (active.id !== over?.id) {
-      setComponents((items) => {
-        const oldIndex = items.findIndex((item) => item.id === active.id);
-        const newIndex = items.findIndex((item) => item.id === over?.id);
-
-        return arrayMove(items, oldIndex, newIndex);
-      });
-    }
-
-    setActive(null);
-  }
-
-  function handleDragCancel() {
-    setActive(null);
   }
 
   return (
@@ -59,29 +16,13 @@ export default function Editor() {
 
       <main className="p-4 text-gray-800 flex-1">
         <h2 className="text font-bold">Editor</h2>
-        <div className="w-full flex items-center justify-center">
+        <div className="w-full flex flex-col items-center justify-center">
           <div className="p-4 m-4 w-96 border-2 border-dashed border-gray-400 rounded">
             <div className="grid grid-cols-4 gap-2">
-              <DndContext
-                sensors={sensors}
-                // collisionDetection={closestCenter}
-                onDragStart={handleDragStart}
-                onDragEnd={handleDragEnd}
-                onDragCancel={handleDragCancel}
-              >
-                <SortableContext
-                  items={components.map((_, idx) => idx)}
-                  strategy={rectSortingStrategy}
-                >
-                  {components.map((item) => (
-                    <SortableItem key={item.id} item={item} />
-                  ))}
-                </SortableContext>
-
-                <DragOverlay adjustScale>
-                  {active ? <SortableItem item={active} /> : null}
-                </DragOverlay>
-              </DndContext>
+              <SortableArea
+                components={components}
+                setComponents={setComponents}
+              />
             </div>
           </div>
         </div>
---
+++ src/components/SortableArea.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import {
+  DndContext,
+  DragEndEvent,
+  DragOverlay,
+  DragStartEvent,
+  PointerSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  rectSortingStrategy,
+  SortableContext,
+} from "@dnd-kit/sortable";
+import type { Component } from "../types";
+import SortableItem from "./SortableItem";
+
+type Props = {
+  components: Component[];
+  setComponents: React.Dispatch<React.SetStateAction<Component[]>>;
+};
+
+export default function SortableArea({ components, setComponents }: Props) {
+  const [active, setActive] = useState<Component | null>(null);
+
+  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+
+    setActive(components.find((item) => item.id === active.id) ?? null);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    if (active.id !== over?.id) {
+      setComponents((items) => {
+        const oldIndex = items.findIndex((item) => item.id === active.id);
+        const newIndex = items.findIndex((item) => item.id === over?.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+
+    setActive(null);
+  }
+
+  function handleDragCancel() {
+    setActive(null);
+  }
+  return (
+    <DndContext
+      sensors={sensors}
+      // collisionDetection={closestCenter}
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}
+      onDragCancel={handleDragCancel}
+    >
+      <SortableContext
+        items={components.map((_, idx) => idx)}
+        strategy={rectSortingStrategy}
+      >
+        {components.map((item) => (
+          <SortableItem key={item.id} item={item} />
+        ))}
+      </SortableContext>
+
+      <DragOverlay adjustScale>
+        {active ? <SortableItem item={active} /> : null}
+      </DragOverlay>
+    </DndContext>
+  );
+}
